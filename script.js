ea461dfee40c8d98ce82f236818259deeaddbb54
Jakub Molinski <jakubmolinski9@gmail.com>
2024-03-16 16:05:21 +0100

    Small css adjustments

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const list = document.createElement("ul");
 
 // Add your code here
 
-
-
-
 // Don't edit the code below here!
 const section = document.querySelector("section");
 section.appendChild(list);
